5ffa35220c1f9180070eaa20820ffc95a8d3710a
Tyler <tyler_venetsanos@msn.com>
2021-04-09 12:06:32 -0400

    commit tree 2d652b73748c0b6e0e26ee07063e7681e31d5573

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution here
+function findMatching(drivers, name) {
+    return drivers.filter(x => x.toLowerCase() === name.toLowerCase())
+  }
+
+  function fuzzyMatch(drivers, string) {
+      return drivers.filter(x => x.slice(0, string.length) == string)
+  }
+
+  function matchName(drivers, hometown) {
+      return drivers.filter(x => x.name == hometown)
+  }
